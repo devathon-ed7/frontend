4e2166b021170447c438024eb22b38eb0187fa77
Raydberg Gabriel Chuquival Gil <raydga_2005@hotmail.com>
2024-07-24 12:01:21 -0500

    chore: add form validations

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,31 +1,57 @@
-import { FormEvent, useState } from "react";
+import { FormEvent, useState, useCallback } from "react";
 import { InputField } from "../Input/Input";
 
 export const LoginForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [rememberMe, setRememberMe] = useState(false);
+  const [errors, setErrors] = useState({ email: "", password: "" });
 
-  const handleSubmit = (e: FormEvent) => {
-    e.preventDefault();
-    console.log({ email, password, rememberMe });
-  };
+  const validateForm = useCallback(() => {
+    const newErrors = { email: "", password: "" };
+    let hasErrors = false;
+    if (!email.trim()) {
+      newErrors.email = "El correo electrónico es obligatorio.";
+      hasErrors = true;
+    }
+    if (!password.trim()) {
+      newErrors.password = "La contraseña es obligatoria.";
+      hasErrors = true;
+    }
 
+    setErrors(newErrors);
+    return !hasErrors;
+  }, [email, password]);
+
+  const handleSubmit = useCallback(
+    (e: FormEvent) => {
+      e.preventDefault();
+
+      if (validateForm()) {
+        console.log({ email, password, rememberMe });
+      }
+    },
+    [email, password, rememberMe, validateForm],
+  );
   return (
     <form onSubmit={handleSubmit} className="flex flex-col gap-3 sm:gap-5">
       <InputField
         id="email"
-        type="text"
-        placeholder="Correo Electrónico"
+        type="email"
+        title="Correo Electrónico"
+        placeholder="correo@example.com"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
+        errorMessage={errors.email}
       />
       <InputField
         id="password"
         type="password"
-        placeholder="Contraseña"
+        title="Contraseña"
+        placeholder="*******"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
+        errorMessage={errors.password}
       />
       <div className="flex flex-col gap-4">
         <div className="flex items-center justify-between sm:justify-start">

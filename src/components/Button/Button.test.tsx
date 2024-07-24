dea43f3e3e915b8bc9e73c6be02a772b723b70e4
Raydberg Gabriel Chuquival Gil <raydga_2005@hotmail.com>
2024-07-24 12:01:21 -0500

    chore: add form validations

---
+++ src/components/Button/Button.test.tsx
@@ -0,0 +1,19 @@
+// import { render, screen } from "@testing-library/react";
+// import { Button } from "./Button";
+
+// describe("Button Component", () => {
+//   // Test if the Button component renders correctly
+//   it("renders without crashing", () => {
+//     render(<Button title="Test Button" />);
+//     const buttonElement = screen.getByText(/Test Button/i);
+//     expect(buttonElement).toBeInTheDocument();
+//   });
+
+//   // Test if the Button component displays the correct title
+//   it("displays the correct title", () => {
+//     const testTitle = "Click Me!";
+//     render(<Button title={testTitle} />);
+//     const buttonElement = screen.getByText(testTitle);
+//     expect(buttonElement).toBeInTheDocument();
+//   });
+// });
--- src/components/Input/Input.types.ts
+++ src/components/Input/Input.types.ts
@@ -2,6 +2,7 @@ export interface InputFieldProps {
   id: string;
   label: string;
   type: string;
+  title: string;
   placeholder: string;
   error?: string;
 }

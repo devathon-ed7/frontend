853dbea6a822f57b8e23a168d0ba527f4b095958
Bolivar Delgado <bolivar.delgado@gmail.com>
2024-08-01 14:18:39 -0500

    feat: add style userView

--- src/views/UserView/UserView.tsx
+++ src/views/UserView/UserView.tsx
@@ -1,8 +1,11 @@
 export const UserView = () => {
   return (
     <div>
-      <div className="sticky top-0 z-50 h-10 bg-gray-200">
-        <h1>User View</h1>
+      <div className="sticky top-0 z-50 flex h-16 items-center justify-between border-b border-neutral-200 p-4">
+        <h1>Usuarios</h1>
+        <div>
+          <button className="rounded-md bg-blue-500 px-2 py-1 text-sm text-white">+ Nuevo</button>
+        </div>
       </div>
     </div>
   );

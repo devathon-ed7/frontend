6d1bb5acf9746fcdc1d859545bd34861f232c0b1
Bolivar Delgado <bolivar.delgado@gmail.com>
2024-08-02 10:48:48 -0500

    feat: style card user

--- src/views/UserView/UserView.tsx
+++ src/views/UserView/UserView.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import usersService from "../../services/users";
+import "./UserView.css";
 
 export const UserView = () => {
   const [users, setUsers] = useState([]);
@@ -13,16 +14,29 @@ export const UserView = () => {
   }, []);
 
   return (
-    <div>
-      <div className="sticky top-0 z-50 flex h-16 items-center justify-between border-b border-neutral-200 p-4">
+    <div className="flex h-full flex-col">
+      <div className="sticky top-0 z-50 flex h-16 w-full items-center justify-between border-b border-neutral-200 p-4">
         <h1>Usuarios</h1>
         <div>
           <button className="rounded-md bg-blue-500 px-2 py-1 text-sm text-white">+ Nuevo</button>
         </div>
       </div>
       {/* content users list */}
-
-      <pre>{JSON.stringify(users, null, 2)}</pre>
+      <div className="flex h-full w-full flex-wrap p-8">
+        {users.map((user) => (
+          <div
+            key={user.id}
+            className="m-4 flex h-60 w-60 flex-col border-neutral-200 p-4 shadow-xl"
+          >
+            <span className="avatar-icon self-center"></span>
+            <div className="flex items-center justify-between">
+              <div>
+                <h2 className="text-lg">{user.username}</h2>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };

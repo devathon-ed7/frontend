f67793e0739d607b2344264d62c6d8c4179606c3
Bolivar Delgado <bolivar.delgado@gmail.com>
2024-08-03 00:33:01 -0500

    feat: add base createview

--- src/views/UserView/UserView.tsx
+++ src/views/UserView/UserView.tsx
@@ -18,7 +18,7 @@ export const UserView = () => {
       <div className="sticky top-0 z-50 flex h-16 w-full items-center justify-between border-b border-neutral-200 p-4">
         <h1>Usuarios</h1>
         <div>
-          <button className="rounded-md bg-blue-500 px-2 py-1 text-sm text-white">+ Nuevo</button>
+          <button className="rounded-md bg-blue-500 px-2 py-2 text-sm text-white">+ Nuevo</button>
         </div>
       </div>
       {/* content users list */}

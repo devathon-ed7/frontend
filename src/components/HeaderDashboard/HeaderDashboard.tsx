baff84b380851087b99455532f5f08201e7ae924
Bolivar Delgado <bolivar.delgado@gmail.com>
2024-08-04 12:51:47 -0500

    feat: logout

--- src/components/HeaderDashboard/HeaderDashboard.tsx
+++ src/components/HeaderDashboard/HeaderDashboard.tsx
@@ -1,11 +1,21 @@
 import "./HeaderDashboard.css";
+import useLogin from "../../hooks/useLogin";
 
 export const HeaderDashboard = () => {
+  const { logout } = useLogin();
+
+  const handleLogout = () => {
+    logout();
+  };
+
   return (
     <header className="header">
       <div className="logo-container">
         <span className="logo"></span>
         <span>Sistema de Inventario</span>
+      </div>
+      <div className="mx-4 flex cursor-pointer items-center">
+        <span className="logout-icon" onClick={handleLogout}></span>
       </div>
     </header>
   );
